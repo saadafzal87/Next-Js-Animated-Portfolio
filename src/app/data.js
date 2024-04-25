a2dd6fc9d200829227e770062e30af542560e9cc
Saad Afzal <126021044+saadabcafzal@users.noreply.github.com>
2024-04-25 19:51:48 +0500

    Personal Portfolio Finalized

---
+++ src/app/data.js
@@ -0,0 +1,91 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Study Notion",
+    description: "An Ed-tech Platform for learning",
+    date: "2022-08-15",
+    demoLink: "https://github.com/saadabcafzal/StudyNotion",
+  },
+  {
+    id: 2,
+    name: "DevOverFlow",
+    description: "A problem solving community platform based application",
+    date: "2022-06-20",
+    demoLink: "https://github.com/saadabcafzal/DevOverFlow",
+  },
+  {
+    id: 3,
+    name: "Ecommerce",
+    description: "AdminDashboard + CMS Services",
+    date: "2022-09-10",
+    demoLink: "https://github.com/saadabcafzal/ecommerce_admin",
+  },
+  {
+    id: 4,
+    name: "Real Time Chat App",
+    description: "implemented real-time chat functionality",
+    date: "2022-05-30",
+    demoLink: "https://github.com/saadabcafzal/ChatApp_Backend",
+  },
+  {
+    id: 5,
+    name: "MovieApp",
+    description: "Web application for browsing and searching movies, built using React.",
+    date: "2022-07-12",
+    demoLink: "https://github.com/saadabcafzal/REACT_MOVIE_APP",
+  },
+  {
+    id: 6,
+    name: "Refokus Website",
+    description: "forward-thinking web agency that combines Web Design, Webflow Development, and Creative Development",
+    date: "2022-10-01",
+    demoLink: "https://github.com/saadabcafzal/Refukus_landing_Page",
+  },
+  {
+    id: 7,
+    name: "Obys Website",
+    description: "Digital design agency focused on creative UI/UX design, branding and interaction design.",
+    date: "2022-04-18",
+    demoLink: "https://github.com/saadabcafzal/Obys-Landing-Page",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/saadabcafzal",
+    icon: "github",
+    newTab: true,
+  },
+ 
+  {
+    label: "Resume",
+    link: "https://brown-lyndell-45.tiiny.site/",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
